04311426005342f69dfee779f576864ea44c53c6
Marco Alejandro Ramírez <ma.ramirez23@uniandes.edu.co>
2024-05-14 20:26:05 -0500

    credentials

--- CRM/website/views.py
+++ CRM/website/views.py
@@ -143,11 +143,8 @@ def pedirProducto(request, pk):
 
 
 def sendWhatsapp(user, producto):
-    important = ImportantStuff.objects.get(id=1)
-    a = important.account_sid
-    b = important.auth_token
-    print(a)
-    print(b)
+    a = "ACc73bf418b9f02cb52e76da46813005e4"
+    b = "59ca98923bfeb28dba728a1a140596f0"
 
     try:
         client = Client(a, b)
